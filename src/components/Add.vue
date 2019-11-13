f1f2260fff62979f3c8ffde4456a2122ef2f72f8
TonightAC <suresursus@outlook.com>
2019-11-13 18:05:57 +0800

    11/13 18:05

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -9,17 +9,27 @@
         <el-main class="el-main">
             <el-row>
                 <el-col style="padding-right: 3px" :span="18"><el-input v-model="searchString" placeholder="PP号/昵称"></el-input></el-col>
-                <el-col style="padding-left: 3px" :span="6"><el-button v-loading="loading" style="width: 100%;" @click="search" type="primary">搜索</el-button></el-col>
+                <el-col style="padding-left: 3px" :span="6"><el-button style="width: 100%;" @click="search" type="primary">搜索</el-button></el-col>
             </el-row>
             <div v-for="item in searchList" :key="item.index">
                 <el-card style="border-top: 0; border-left: 0; border-right: 0; position: relative;" :body-style="{ padding: '20px' }" shadow="never">
                     <el-avatar style="position: absolute; left: 10px; top: 50%; transform: translateY(-50%)" size="large" src="https://empty">
                         <img src="../assets/avatar/avatar1.png" alt="avatar"/>
                     </el-avatar>
-                    <span style="margin-left: 50px">{{ item.nickname }}</span>
+                    <div style="margin-left: 45px">{{ item.nickname }}</div>
+                    <div style="margin-left: 45px; font-size: 12px">{{ item.ppid }}</div>
+                    <i @click="clickItem(item)" style="position: absolute; right: 20px; top: 50%; transform: translateY(-50%); font-size: 20px" class="el-icon-circle-plus-outline"></i>
                 </el-card>
             </div>
         </el-main>
+        <el-dialog :visible.sync="dialogVisible" width="75%">
+            <h4>请输入添加验证信息：</h4>
+            <el-input v-model="verifyMessage" placeholder="请输入内容"></el-input>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="dialogVisible = false">取消</el-button>
+                <el-button type="primary" @click="add">添加</el-button>
+            </span>
+        </el-dialog>
     </el-container>
 </template>
 
@@ -29,35 +39,18 @@
         name: "Add",
         data () {
             return {
+                verifyMessage: '',
                 searchString: '',
-                loading: false,
-                searchList: []
+                item: null,
+                searchList: [],
+                dialogVisible: false
             }
-        },
-        created () {
-            // this.searchList.push({nickname: '123'});
-            // this.searchList.push({nickname: '123'});
-            // this.searchList.push({nickname: '123'});
-            // this.searchList.push({nickname: '123'});
-            // this.searchList.push({nickname: '123'});
-            // this.searchList.push({nickname: '123'});
-            // this.searchList.push({nickname: '123'});
-            // this.searchList.push({nickname: '123'});
-            // this.searchList.push({nickname: '123'});
-            // this.searchList.push({nickname: '123'});
-            // this.searchList.push({nickname: '123'});
-            // this.searchList.push({nickname: '123'});
-            // this.searchList.push({nickname: '123'});
-            // this.searchList.push({nickname: '123'});
-            // this.searchList.push({nickname: '123'});
-            // this.searchList.push({nickname: '123'});
         },
         methods: {
             search () {
                 if(this.searchString.length !== 0){
-                    this.loading = true;
                     this.axios.get('/user/search?searchString=' + this.searchString).then(res => {
-                        this.loading = false;
+                        this.searchList.length = 0;
                         if(res.data.code === '0000'){
                             if(res.data.data.length !== 0){
                                 for (let i = 0; i < res.data.data.length; i++){
@@ -82,7 +75,6 @@
                             });
                         }
                     }).catch(error => {
-                        this.loading = false;
                         Toast({
                             message: '网络异常',
                             position: 'bottom',
@@ -98,6 +90,37 @@
                         duration: 2000
                     });
                 }
+            },
+            add () {
+                this.dialogVisible = false;
+                this.axios.post('/user/add?ppid=' + this.item.ppid + '&uid1=' + this.item.uid + '&uid2=' + sessionStorage.getItem('uid') + '&verifyMessage=' + this.verifyMessage).then(res => {
+                    this.searchList.length = 0;
+                    if(res.data.code === '0000'){
+                        Toast({
+                            message: '添加成功，等待对方确认',
+                            position: 'bottom',
+                            duration: 2000
+                        });
+                    } else {
+                        Toast({
+                            message: '添加失败',
+                            position: 'bottom',
+                            duration: 2000
+                        });
+                    }
+                }).catch(error => {
+                    Toast({
+                        message: '网络异常',
+                        position: 'bottom',
+                        duration: 2000
+                    });
+                    // eslint-disable-next-line no-console
+                    console.log(error);
+                });
+            },
+            clickItem (item) {
+                this.dialogVisible = true;
+                this.item = item;
             },
             back () {
                 this.$emit('switchView', {chatShow: false, homeShow: true, addShow: false, settingShow: false});
